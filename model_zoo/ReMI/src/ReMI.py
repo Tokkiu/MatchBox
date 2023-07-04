d32a09e9841bc44e0f51605f0686d22b7216d673
Tokkiu <mrgao.ary@gmail.com>
2023-07-04 21:34:24 +0800

    add remi

--- model_zoo/ReMI/src/ReMI.py
+++ model_zoo/ReMI/src/ReMI.py
@@ -133,7 +133,6 @@ class ComiRecAggregator(nn.Module):
             nn.Tanh()
         )
         self.linear2 = nn.Linear(self.hidden_size * 4, self.num_heads, bias=False)
-        # self.reset_parameters()
 
     def forward(self, item_eb, label_eb, mask):
         # item_eb = self.embeddings(item_list)
@@ -150,7 +149,7 @@ class ComiRecAggregator(nn.Module):
         item_att_w = torch.transpose(item_att_w, 2, 1).contiguous()
 
         atten_mask = torch.unsqueeze(mask, dim=1).repeat(1, self.num_heads, 1)  # shape=(batch_size, num_heads, maxlen)
-        paddings = torch.ones_like(atten_mask, dtype=torch.float) * (-2 ** 32 + 1)  # softmax之后无限接近于0
+        paddings = torch.ones_like(atten_mask, dtype=torch.float) * (-2 ** 32 + 1)
 
         item_att_w = torch.where(torch.eq(atten_mask, 0), paddings, item_att_w)
         item_att_w = F.softmax(item_att_w, dim=-1)  # 矩阵A，shape=(batch_size, num_heads, maxlen)
@@ -167,8 +166,6 @@ class ComiRecAggregator(nn.Module):
         return user_eb, item_att_w
 
     def read_out(self, user_eb, label_eb):
-
-        # 这个模型训练过程中label是可见的，此处的item_eb就是label物品的嵌入
         atten = torch.matmul(user_eb,  # shape=(batch_size, interest_num, hidden_size)
                              torch.reshape(label_eb, (-1, self.hidden_size, 1))  # shape=(batch_size, hidden_size, 1)
                              )  # shape=(batch_size, interest_num, 1)
@@ -176,17 +173,16 @@ class ComiRecAggregator(nn.Module):
         atten = F.softmax(torch.pow(torch.reshape(atten, (-1, self.interest_num)), 1),
                           dim=-1)  # shape=(batch_size, interest_num)
 
-        if self.hard_readout:  # 选取interest_num个兴趣胶囊中的一个，MIND和ComiRec都是用的这种方式
+        if self.hard_readout:
             readout = torch.reshape(user_eb, (-1, self.hidden_size))[
                 (torch.argmax(atten, dim=-1) + torch.arange(label_eb.shape[0],
                                                             device=user_eb.device) * self.interest_num).long()]
-        else:  # 综合interest_num个兴趣胶囊，论文及代码实现中没有使用这种方法
+        else:
             readout = torch.matmul(torch.reshape(atten, (label_eb.shape[0], 1, self.interest_num)),
                                    # shape=(batch_size, 1, interest_num)
                                    user_eb  # shape=(batch_size, interest_num, hidden_size)
                                    )  # shape=(batch_size, 1, hidden_size)
             readout = torch.reshape(readout, (label_eb.shape[0], self.hidden_size))  # shape=(batch_size, hidden_size)
-        # readout是vu堆叠成的矩阵（一个batch的vu）（vu可以说就是最终的用户嵌入）
         selection = torch.argmax(atten, dim=-1)
         return readout, selection
 
